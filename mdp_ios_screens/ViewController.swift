ed3f95a639dccd127892f0051fff1c730e2cdd60
Alyona <alena.hulak@gmail.com>
2018-08-15 16:49:10 +0300

    Initial Commit

---
+++ mdp_ios_screens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mdp_ios_screens
+//
+//  Created by Alyona Hulak on 8/15/18.
+//  Copyright © 2018 Alyona Hulak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
